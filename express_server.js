40b25d1cf7fb61a023172578055d01a38b399d8b
Lisa Wild <lisakwild@gmail.com>
2024-02-27 01:46:32 -0800

    Add comments to identify new routes purpose

--- express_server.js
+++ express_server.js
@@ -18,11 +18,13 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 })
 
+// makes urlDatabase accessible to urls_index
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 })
 
+// makes id (shortened URL) and long URL accessible to urls_show
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]}
   res.render('urls_show', templateVars)
