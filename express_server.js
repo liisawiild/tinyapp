9aa62b9be183d3784345a5d25b134de4c6104d28
Lisa Wild <lisakwild@gmail.com>
2024-02-27 10:02:04 -0800

    Add error redirect if the short URL not in database

--- express_server.js
+++ express_server.js
@@ -18,48 +18,52 @@ app.get("/", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-})
+});
 
 // makes urlDatabase accessible to urls_index
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-})
+});
 
-// error message page for no ID/long URL found 
+// error message page for no ID/long URL found
 app.get('/u/error', (req, res) => {
-  res.send('The requested URL does not exist.')
-})
+  res.send('The requested URL does not exist.');
+});
 
 // responds to a POST with redirect to page with new id and input longURL
 app.post("/urls", (req, res) => {
   const id = generateRandomString();
   const longURL = req.body.longURL;
-  urlDatabase[id] = req.body.longURL; 
+  urlDatabase[id] = req.body.longURL;
   res.redirect(`/urls/${id}`);
-})
+});
 
 // render the new tiny URL form
 app.get("/urls/new", (req, res) => {
   res.render('urls_new');
-})
+});
 
 // makes id (shortened URL) and long URL accessible to urls_show
 app.get('/urls/:id', (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]}
-  res.render('urls_show', templateVars)
-})
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render('urls_show', templateVars);
+});
 
 // click shortURL or request /u/shortURL = redirect to longURL
 app.get("/u/:id", (req, res) => {
-  const id = req.params.id;   
-  const longURL = urlDatabase[id];  
-  return res.redirect(longURL);
-})
+  const id = req.params.id;
+  if (urlDatabase[id] === undefined) {
+    return res.redirect('/u/error');
+  } else {
+    const longURL = urlDatabase[id];
+    return res.redirect(longURL);
+  }
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -67,7 +71,7 @@ app.listen(PORT, () => {
 
 
 // generate a random short URL ID
-function generateRandomString() {
+const generateRandomString = function() {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWYZabcdefghijklmnopqrstuvwxyz0123456789';
   let shortURL = "";
   for (let i = 0; i < 6; i++) {
