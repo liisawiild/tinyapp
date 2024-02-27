033752a73ef49585f66a59a1c7195c235c27939e
Lisa Wild <lisakwild@gmail.com>
2024-02-26 23:47:55 -0800

    Add route for /urls which connects & provides urlDatabase obj to urls_index template

--- express_server.js
+++ express_server.js
@@ -15,7 +15,12 @@ app.get("/", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+})
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 })
 
 app.get("/urls.json", (req, res) => {
