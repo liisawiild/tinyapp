064c1bbbc64bf9ac501fc9275d22d2b19ffc4c59
Lisa Wild <lisakwild@gmail.com>
2024-03-07 17:05:04 -0800

    Move helper functions into a helper.js file and export them

---
+++ helpers.js
@@ -0,0 +1,21 @@
+// generate a random short URL ID
+const generateRandomString = function() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let shortURL = "";
+  for (let i = 0; i < 6; i++) {
+    shortURL += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return shortURL;
+};
+
+// lookup user in database by email
+const getUserByEmail = function(userEmail, database) {
+  for (let user in database) {
+    if (database[user].email === userEmail) {
+      return database[user];
+    }
+  }
+  return null;
+};
+
+module.exports = { generateRandomString, getUserByEmail }
